3158e23367257cc06798b7e3da1f9e2bb6ad146f
Nathan Halash <nhalash@emich.edu>
2024-02-16 00:08:58 -0500

    updating files

--- php-scripts/pdo.php
+++ php-scripts/pdo.php
@@ -1,3 +1,10 @@
 <?php 
 // make connection to data base using PDO object
-$pdo = new PDO('mysql:host=demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com;port=3306;dbname=testDB', 'admin', 'Password123');
+try {
+    $pdo = new PDO('mysql:host=demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com;port=3306;dbname=demoDatabase471','admin','Password123');
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
--- php-scripts/user_signup.php
+++ php-scripts/user_signup.php
@@ -1,10 +1,16 @@
 <?php
+// error log imports:
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // import pdo.php to make connection to DB:
 require_once "pdo.php";
+
 // use iffset() to see if fields have been filled in the HTML signup page:
-if(isset($_POST['fname']) && isset($_POST_['lname']) && isset($_POST_['username'])  
-            && isset($_POST_['phone'])  && isset($_POST_['email'])  
-            && isset($_POST_['password'])) 
+if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['username'])  
+            && isset($_POST['phone'])  && isset($_POST['email'])  
+            && isset($_POST['password'])) 
         {
             // Write teh SQL query:
             $sql = "INSERT INTO user (fname, lname, username, phone, email, password)
@@ -23,4 +29,5 @@ if(isset($_POST['fname']) && isset($_POST_['lname']) && isset($_POST_['username'
                 'password' => $_POST['password']));
         }
     // Pray that it works lmao
+    header('Location: ../signup.html');
 ?>
