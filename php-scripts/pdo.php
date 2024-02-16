f58dbb70479bd1892cc3071f5fcc47115ff2e38f
Nathan Halash <nhalash@emich.edu>
2024-02-15 21:32:07 -0500

    fixed pdo.php 2/15/24 9:31pm

--- php-scripts/pdo.php
+++ php-scripts/pdo.php
@@ -1,16 +1,3 @@
 <?php 
 // make connection to data base using PDO object
-// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'nhalash', '20012766');
-
-
-$host="demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com";
-$port=3306;
-$socket="";
-$user="admin";
-$password="";
-$dbname="testDB";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
+$pdo = new PDO('mysql:host=demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com;port=3306;dbname=testDB', 'admin', 'Password123');
