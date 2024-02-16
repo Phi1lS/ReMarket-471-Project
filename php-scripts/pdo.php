5e3a8522172851d14aab27e5d84ef6447c3dd76c
Nathan Halash <nhalash@emich.edu>
2024-02-15 21:11:16 -0500

    updated pdo.php to connect to new DB

--- php-scripts/pdo.php
+++ php-scripts/pdo.php
@@ -1,4 +1,16 @@
 <?php 
 // make connection to data base using PDO object
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'nhalash', '20012766');
-// see the "errors" folder for details...
+// $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'nhalash', '20012766');
+
+
+$host="demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com";
+$port=3306;
+$socket="";
+$user="admin";
+$password="";
+$dbname="testDB";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
