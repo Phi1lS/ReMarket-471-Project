c90dbf74a33b391db369a4f6788f05080df9d084
Nathan Halash <nhalash@emich.edu>
2024-02-15 23:00:04 -0500

    fixed flip flop issue w/ login/signup scripts

--- php-scripts/user_login.php
+++ php-scripts/user_login.php
@@ -1,26 +1,3 @@
 <?php
-// import pdo.php to make connection to DB:
-require_once "pdo.php";
-// use iffset() to see if fields have been filled in the HTML signup page:
-if(isset($_POST['fname']) && isset($_POST_['lname']) && isset($_POST_['username'])  
-            && isset($_POST_['phone'])  && isset($_POST_['email'])  
-            && isset($_POST_['password'])) 
-        {
-            // Write teh SQL query:
-            $sql = "INSERT INTO user (fname, lname, username, phone, email, password)
-                    VALUES (:fname, :lname, :username, :phone, :email, :password)";
-
-            // Parse the data from above and prepare it:
-            $stmt = $pdo->prepare($sql);
-
-            // Execute the Query into the DB:
-            $stmt->execute(array(
-                'fname' => $_POST['fname'],
-                'lname' => $_POST['lname'],
-                'username' => $_POST['username'],
-                'phone' => $_POST['phone'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password']));
-        }
-    // Pray that it works lmao
+// refer to user_signup.php for template.
 ?>
--- php-scripts/user_signup.php
+++ php-scripts/user_signup.php
@@ -1,15 +1,26 @@
 <?php
-// This is all placeholder code for now until it's covered in class
-// Placeholder for signup logic
-if (isset($_POST['signup'])) {
-    // TODO: Logic to handle signup
+// import pdo.php to make connection to DB:
+require_once "pdo.php";
+// use iffset() to see if fields have been filled in the HTML signup page:
+if(isset($_POST['fname']) && isset($_POST_['lname']) && isset($_POST_['username'])  
+            && isset($_POST_['phone'])  && isset($_POST_['email'])  
+            && isset($_POST_['password'])) 
+        {
+            // Write teh SQL query:
+            $sql = "INSERT INTO user (fname, lname, username, phone, email, password)
+                    VALUES (:fname, :lname, :username, :phone, :email, :password)";
 
-    // Redirect to the login page for now
-    header('Location: login.html');
-    exit();
-}
+            // Parse the data from above and prepare it:
+            $stmt = $pdo->prepare($sql);
 
-// If the form wasn't submitted, redirect to the signup form
-header('Location: signup.html');
-exit();
+            // Execute the Query into the DB:
+            $stmt->execute(array(
+                'fname' => $_POST['fname'],
+                'lname' => $_POST['lname'],
+                'username' => $_POST['username'],
+                'phone' => $_POST['phone'],
+                'email' => $_POST['email'],
+                'password' => $_POST['password']));
+        }
+    // Pray that it works lmao
 ?>
