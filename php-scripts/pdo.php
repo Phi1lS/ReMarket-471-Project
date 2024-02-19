1425a49ff681539078a57b615ccbf807fa3beeea
Nathan Halash <nhalash@emich.edu>
2024-02-19 15:18:00 -0500

    making change to pdo.php so it connects to localhost, AWS is broken

--- php-scripts/pdo.php
+++ php-scripts/pdo.php
@@ -1,10 +1,17 @@
 <?php 
 // make connection to data base using PDO object
+$host = "localhost";
+$port = 3306;
+$user = "root";
+$password = "";
+$dbname = "remarket";
+
 try {
-    $pdo = new PDO('mysql:host=demodatabase471.c9as2uq8gw4v.us-east-1.rds.amazonaws.com;port=3306;dbname=demoDatabase471','admin','Password123');
-    // set the PDO error mode to exception
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+
 ?>
