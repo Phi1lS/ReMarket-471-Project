48b303ca4446a94befa273a35d01f2fc867f1074
Nathan Halash <nhalash@emich.edu>
2024-02-21 22:31:58 -0500

    added script that checks if user login credentials are correct

--- php-scripts/user_login.php
+++ php-scripts/user_login.php
@@ -1,3 +1,38 @@
 <?php
-// refer to user_signup.php for template.
+// error log imports:
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// import pdo.php to make connection to DB:
+require_once "pdo.php";
+
+// Use isset() to see if the follwoing fields have been entered in HTML:
+// 1. Username, 2. Password
+if(isset($_POST['username']) && isset($_POST['password'])) 
+    {
+        // Write the SQL query to check if values (uername/password) exist:
+            $sql = "SELECT username, password
+            FROM user WHERE username = :username AND password = :password";
+
+        // Prepare the SQL statement:
+        $stmt = $pdo->prepare($sql);
+
+        // Execute the SQL statement, passing in the username and password
+        $stmt->execute(array(
+            ':username' => $_POST['username'],
+            ':password' => $_POST['password']
+        ));
+
+        // Get the results:
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Check the results:
+        if($row === false) {
+            echo "Login failed... Try again";
+        }
+        else {
+            echo "Login was succesfull :)";
+        }
+    }
 ?>
