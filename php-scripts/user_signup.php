3f80c6d2557d88154d783de010c2a23feed9142d
Nathan Halash <nhalash@emich.edu>
2024-03-03 22:07:53 -0500

    added user sessions

--- php-scripts/user_signup.php
+++ php-scripts/user_signup.php
@@ -1,33 +1,42 @@
 <?php
-// error log imports:
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Start a session at the beginning of your PHP script:
+session_start();
 
-// import pdo.php to make connection to DB:
+// Import pdo.php to make a connection to the database:
 require_once "pdo.php";
 
-// use iffset() to see if fields have been filled in the HTML signup page:
-if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['username'])  
-            && isset($_POST['phone'])  && isset($_POST['email'])  
-            && isset($_POST['password'])) 
-        {
-            // Write teh SQL query:
-            $sql = "INSERT INTO user (fname, lname, username, phone, email, password)
-                    VALUES (:fname, :lname, :username, :phone, :email, :password)";
+// Use isset() to check if fields have been filled in the HTML signup page:
+if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['username'])  
+    && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['password'])) 
+{
+    // Write the SQL query to insert user data into the 'user' table:
+    $sql = "INSERT INTO user (fname, lname, username, phone, email, password)
+            VALUES (:fname, :lname, :username, :phone, :email, :password)";
 
-            // Parse the data from above and prepare it:
-            $stmt = $pdo->prepare($sql);
+    // Parse the data and prepare the query:
+    $stmt = $pdo->prepare($sql);
 
-            // Execute the Query into the DB:
-            $stmt->execute(array(
-                'fname' => $_POST['fname'],
-                'lname' => $_POST['lname'],
-                'username' => $_POST['username'],
-                'phone' => $_POST['phone'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password']));
-        }
-    // Pray that it works lmao
+    // Execute the query into the database:
+    $stmt->execute(array(
+        'fname' => $_POST['fname'],
+        'lname' => $_POST['lname'],
+        'username' => $_POST['username'],
+        'phone' => $_POST['phone'],
+        'email' => $_POST['email'],
+        'password' => $_POST['password']));
+
+    // Retrieve the user ID after successful insertion (assuming 'id' is an auto-increment primary key):
+    $user_id = $pdo->lastInsertId();
+
+    // Store the user ID in the session:
+    $_SESSION['user_id'] = $user_id;
+
+    // Insert the user ID into the 'active_users' table:
+    $sql_active_users = "INSERT INTO active_users (user_id) VALUES (:user_id)";
+    $stmt_active_users = $pdo->prepare($sql_active_users);
+    $stmt_active_users->execute(array('user_id' => $user_id));
+
+    // Redirect the user to the desired page (e.g., index.html):
     header('Location: ../index.html');
+}
 ?>
