ee46a7ac9a458683a86dc6622ebcae567902dd45
Nathan Halash <nhalash@emich.edu>
2024-03-28 15:44:14 -0400

    fixed address functionality

---
+++ php-scripts/fetch_address_page.php
@@ -0,0 +1,19 @@
+<?php
+// Start a session at the beginning of your PHP script:
+session_start();
+
+// Include the database connection file
+require_once 'pdo.php';
+
+// Get user_id:
+$user_id = $_SESSION['id'];
+
+$stmt = $pdo->prepare("SELECT * FROM shipping_info WHERE user_id = :user_id");
+$stmt->execute(['user_id' => $user_id]);
+$addresses = [];
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $addresses[] = $row;
+}
+echo json_encode($addresses);
+?>
+
